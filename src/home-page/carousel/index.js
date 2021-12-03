c659dc1d9cb8528295019efad6280360ba55867a
Adriana Lazurca <adrianar.lazurca@gmail.com>
2021-12-03 19:02:17 +0100

    add media queries

--- src/home-page/carousel/index.js
+++ src/home-page/carousel/index.js
@@ -2,14 +2,14 @@ import './carousel.scss';
 
 const Carousel = () => {
    return (
-      <>
-         <div className='carousel'>
+      <div className='carousel'>
+         <div className='carousel-card'>
             <p>
                About me I'm Adriana Lazurca Correia from Portugal! I love programming, as well as running, taking photos
                and learning new things! Feel free to get in touch or take a look at my past work below.
             </p>
          </div>
-      </>
+      </div>
    );
 };
 export default Carousel;
--- src/home-page/presentation-card/index.js
+++ src/home-page/presentation-card/index.js
@@ -2,11 +2,15 @@ import './presentation-card.scss';
 import photo from '../../assets/images/adriana.jpg';
 
 const PresentationCard = () => {
-   return <div className='presentation'>
-      <img src={photo} alt="adriana" width="100" height="100"/>
-      <p>Adriana Lazurca</p>
-      <p>FRONT-END DEVELOPER</p>
-      </div>;
+   return (
+      <div className='presentation'>
+         <img src={photo} alt='adriana' width='100' height='100' />
+         <div >
+            <p>Adriana Lazurca</p>
+            <p>FRONT-END DEVELOPER</p>
+         </div>
+      </div>
+   );
 };
 
 export default PresentationCard;
