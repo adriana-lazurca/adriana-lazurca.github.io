27fde89deca6fbed9cc00c13490751e7f5cb1462
Adriana Lazurca <adrianar.lazurca@gmail.com>
2021-12-08 19:01:11 +0100

    change photo

--- src/home-page/presentation-card/PresentationCard.jsx
+++ src/home-page/presentation-card/PresentationCard.jsx
@@ -1,21 +1,16 @@
 import './presentation-card.scss';
-import smallPhoto from '../../assets/images/adriana-small.jpg';
+import '../../assets/styles/variables/_breakpoints.scss';
 import bigPhoto from '../../assets/images/adriana-big.jpg';
-
 
 export const PresentationCard = () => {
    return (
       <div className='presentation'>
-         <img 
-         src={smallPhoto} alt='adriana' 
-         srcSet={`${smallPhoto} 300w, ${bigPhoto} 1000w`} 
-         // sizes={}
-         />
-         <div >
+         <img src={bigPhoto} alt='adriana' />
+
+         <div>
             <p>Adriana Lazurca</p>
             <p>FRONT-END DEVELOPER</p>
          </div>
       </div>
    );
 };
-
