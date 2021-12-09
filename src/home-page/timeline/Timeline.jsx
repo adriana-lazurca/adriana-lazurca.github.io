2a82e8247322298093f67bbda411638cfbf80212
Adriana Lazurca <adrianar.lazurca@gmail.com>
2021-12-09 20:49:56 +0100

    add timeline

--- src/home-page/timeline/Timeline.jsx
+++ src/home-page/timeline/Timeline.jsx
@@ -3,11 +3,27 @@ import './timeline.scss';
 
 export const Timeline = () => {
    return (
-      <div className='line'>
-         <div className='timeline-container'>
-            <Job />
+      <div className='timeline'>
+         <div className='container left'>
+            <div className='content'>
+               <h2>INTERN FRONT-END</h2>
+               <p>
+                  Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea
+                  mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto
+                  primis ea eam.
+               </p>
+            </div>
+         </div>
+         <div className='container right'>
+            <div className='content'>
+               <h2>INTERN FRONT-END</h2>
+               <p>
+                  Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea
+                  mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto
+                  primis ea eam.
+               </p>
+            </div>
          </div>
       </div>
    );
 };
-
