cbe48f0458acd78eb3defe5ad576162f94f96150
Adriana Lazurca <adrianar.lazurca@gmail.com>
2022-01-06 00:41:35 +0100

    add scss

--- src/home/presentation/Presentation.jsx
+++ src/home/presentation/Presentation.jsx
@@ -4,16 +4,14 @@ import './presentation.scss';
 
 export const Presentation = () => {
    return (
-      <div className='presentation' style={{ display: 'flex', flexDirection: 'column' }}>
-         <img src={bigPhoto} alt='adriana' style={{ height: 30, width: 20 }} />
-         {/* <div style={{ height: '80%' }}>
-            <img src={bigPhoto} alt='adriana' style={{ height: '100%' }} />
+      <div className='presentation'>
+         <div className='presentation__img'>
+            <img src={bigPhoto} alt='adriana' />
          </div>
-
-         <div style={{ height: '20%' }}>
+         <div className='presentation__title'>
             <p>Adriana Lazurca</p>
             <p>FRONT-END DEVELOPER</p>
-         </div> */}
+         </div>
       </div>
    );
 };
