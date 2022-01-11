95776fce7b78293b98167e7f0380ff6b5657d68e
Adriana Lazurca <adrianar.lazurca@gmail.com>
2022-01-11 20:00:37 +0100

    add menu list to carousel

--- src/home/carousel/Carousel.jsx
+++ src/home/carousel/Carousel.jsx
@@ -4,10 +4,18 @@ export const Carousel = () => {
    return (
       <div className='carousel'>
          <div className='carousel__card'>
-            <p>
+            <p className='carousel__card-content'>
                About me: I'm Adriana Lazurca Correia from Romania! I'm Adriana Lazurca Correia from Romania! I'm Adriana
                Lazurca Correia from Romania!I'm Adriana Lazurca Correia from Romania!
             </p>
+            <div className='carousel__card-list'>
+               <ul>
+                  <li>About</li>
+                  <li>Skills</li>
+                  <li>Languages</li>
+                  <li>Interests</li>
+               </ul>
+            </div>
          </div>
       </div>
    );
