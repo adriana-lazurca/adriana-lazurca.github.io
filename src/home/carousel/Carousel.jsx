c24f017419d9c45cbbb51aa3f10fb7c00c19b313
Adriana Lazurca <adrianar.lazurca@gmail.com>
2022-01-12 20:12:16 +0100

    add next and prev buttons

--- src/home/carousel/Carousel.jsx
+++ src/home/carousel/Carousel.jsx
@@ -4,16 +4,20 @@ export const Carousel = () => {
    return (
       <div className='carousel'>
          <div className='carousel__card'>
-            <p className='carousel__card-content'>
-               About me: I'm Adriana Lazurca Correia from Romania! I'm Adriana Lazurca Correia from Romania! I'm Adriana
-               Lazurca Correia from Romania!I'm Adriana Lazurca Correia from Romania!
-            </p>
+            <div className='carousel__card-content'>
+               <p>
+                  About me: I'm Adriana Lazurca Correia from Romania! I'm Adriana Lazurca Correia from Romania! I'm
+                  Adriana Lazurca Correia from Romania!I'm Adriana Lazurca Correia from Romania!
+               </p>
+               <a className='carousel__prev'>&lt;</a>
+               <a className='carousel__next'>&gt;</a>
+            </div>
+
             <div className='carousel__card-list'>
                <ul>
                   <li>About</li>
                   <li>Skills</li>
                   <li>Languages</li>
-                  <li>Interests</li>
                </ul>
             </div>
          </div>
