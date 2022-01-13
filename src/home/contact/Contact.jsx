73f65a27e98a6236f280a5257cc8e68c74ddea75
Adriana Lazurca <adrianar.lazurca@gmail.com>
2022-01-13 19:10:49 +0100

    add icons

--- src/home/contact/Contact.jsx
+++ src/home/contact/Contact.jsx
@@ -1,9 +1,22 @@
 import './contact.scss';
+import { AiOutlineGithub } from 'react-icons/ai';
+import { AiOutlineLinkedin } from 'react-icons/ai';
+import { AiOutlineMail } from 'react-icons/ai';
 
 export const Contact = () => {
    return (
       <div className='contact'>
-         <p>M L F </p>
-      </div>
+            <ul  className='contact__list'>
+               <li>
+                  <AiOutlineGithub color='black'  />
+               </li>
+               <li>
+                  <AiOutlineLinkedin color='black' />
+               </li>
+               <li>
+                  <AiOutlineMail color='black'  />
+               </li>
+            </ul>
+         </div>
    );
 };
--- src/home/navigation/Navigation.jsx
+++ src/home/navigation/Navigation.jsx
@@ -3,7 +3,7 @@ import './navigation.scss';
 export const Navigation = () => (
    <div className='navigation'>
       <ul>
-         <li>Lorem ipsum</li>
+         <li>Home</li>
       </ul>
    </div>
 );
