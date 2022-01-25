9806bf1a32f182d0c2b5eaae5eb6837c0cf3defb
Adriana Lazurca <adrianar.lazurca@gmail.com>
2022-01-25 01:06:02 +0100

    add new component

---
+++ src/home/languages/Languages.jsx
@@ -0,0 +1,10 @@
+// import personalInfo from '../../local-json/personal-info.json';
+
+export const Languages = () => {
+    return (
+       <>
+          <p>Languages</p>
+       </>
+    );
+ };
+ 
