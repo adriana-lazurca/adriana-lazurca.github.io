4e0c5dde3541e5632ef9d6b2ed0b5f00e77c9a3c
Adriana Lazurca <adrianar.lazurca@gmail.com>
2022-01-26 20:15:38 +0100

    add new elements to navigation

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -4,7 +4,8 @@ export const Navigation = () => (
    <div className='navigation'>
       <ul>
          <li>Home</li>
+         <li>Experience</li>
+         <li>Education</li>
       </ul>
    </div>
 );
-
