27f055261e255f8c5745219e60e45392b5af05b5
Adriana Lazurca <adrianar.lazurca@gmail.com>
2022-02-11 17:38:49 +0100

    change display on Languages

--- src/components/presentation/carousel/languages/Languages.jsx
+++ src/components/presentation/carousel/languages/Languages.jsx
@@ -1,16 +1,34 @@
 import { Fragment } from 'react';
+import { MdArrowRightAlt } from 'react-icons/md';
+
 import './languages.scss';
 
 export const Languages = (props) => {
    const { personalInfo } = props;
    return (
       <Fragment>
-         <ul>
-            {personalInfo &&
-               personalInfo.languages.map((language, index) => (
-                  <li key={`language-${language.name}-${index}`}>{`${language.name} - ${language.level}`}</li>
-               ))}
-         </ul>
+         {personalInfo &&
+            personalInfo.languages.map((language, index) => (
+               <div
+                  style={{
+                     display: 'flex',
+                     gap: '50px 30px',
+                     alignItems: 'center',
+                  }}
+               >
+                  <p
+                     style={{
+                        border: '3px solid black',
+                        borderRadius: '30px',
+                        padding: '0.2rem',
+                     }}
+                     key={`language-${language.name}-${index}`}
+                  >
+                     {language.name}
+                  </p>
+                  <span>{` ${language.level}`}</span>
+               </div>
+            ))}
       </Fragment>
    );
 };
