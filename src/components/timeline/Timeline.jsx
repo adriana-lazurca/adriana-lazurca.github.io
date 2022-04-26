d37e70f0f1953d937a62e64fd068a495526a7977
Adriana Lazurca <adrianar.lazurca@gmail.com>
2022-04-26 22:59:06 +0200

    add icons to work and education

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,3 +1,6 @@
+import { MdOutlineWorkOutline } from 'react-icons/md';
+import { MdOutlineSchool } from 'react-icons/md';
+
 import { JobCard } from '../cards/job';
 import { EducationCard } from '../cards/education';
 import { TimelineCard } from './timeline-card';
@@ -9,7 +12,12 @@ export const Timeline = ({ jobs, education }) => {
       <TimelineContainer>
          {jobs &&
             jobs.map((job, index) => (
-               <TimelineCard key={`job-${job.title}-${index}`} index={index} id={index === 0 ? 'experience' : undefined}>
+               <TimelineCard
+                  key={`job-${job.title}-${index}`}
+                  index={index}
+                  id={index === 0 ? 'experience' : undefined}
+                  icon={<MdOutlineWorkOutline />}
+               >
                   <JobCard job={job} />
                </TimelineCard>
             ))}
@@ -19,6 +27,7 @@ export const Timeline = ({ jobs, education }) => {
                   key={`education-${education.title}-${index}`}
                   index={index}
                   id={index === 0 ? 'education' : undefined}
+                  icon={<MdOutlineSchool />}
                >
                   <EducationCard education={education} />
                </TimelineCard>
--- src/components/timeline/timeline-card/TimelineCard.jsx
+++ src/components/timeline/timeline-card/TimelineCard.jsx
@@ -1,6 +1,8 @@
 import './timeline-card.scss';
+// import { MdOutlineWorkOutline } from 'react-icons/md';
+import { MdOutlineSchool } from 'react-icons/md';
 
-export const TimelineCard = ({ children, index, id }) => {
+export const TimelineCard = ({ children, index, id, icon }) => {
    const cardPosition = index % 2 === 0 ? 'left' : 'right';
 
    return (
@@ -9,6 +11,7 @@ export const TimelineCard = ({ children, index, id }) => {
          className={`timeline__card ${cardPosition === 'left' ? 'timeline__card--left' : 'timeline__card--right'}`}
       >
          <div className='timeline__card-content'>{children}</div>
+         <div className='circle'>{icon}</div>
       </div>
    );
 };
